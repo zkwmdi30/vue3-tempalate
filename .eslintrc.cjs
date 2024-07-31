5b7a6efb9cbed91b94fbe9bc02520c47b847888c
이명필 <zkwmdi30@gmail.com>
2024-07-31 15:09:07 +0900

    eslint 설정 추가 및 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,28 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', '@vue/eslint-config-prettier/skip-formatting'],
   overrides: [
     {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/support/**/*.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
+      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended']
     }
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  plugins: ['prettier'],
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    'object-curly-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-in-parens': ['off', 'always'],
+    'space-before-function-paren': ['error', { anonymous: 'always', named: 'never', asyncArrow: 'always' }],
+    indent: 'off',
+    semi: ['error', 'always', { omitLastInOneLineBlock: true }]
   }
-}
+};
